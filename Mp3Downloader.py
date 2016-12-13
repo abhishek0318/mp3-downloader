0f760375e5f7ebdafe5ceab9e224c34cdce84c63
abhishek0318 <abhisheksharma0318@gmail.com>
2016-12-13 23:36:30 +0530

    Added error handling and corrected bugs

--- Mp3Downloader.py
+++ Mp3Downloader.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import wget
+import sys
+import os
 
 def fetch_first_result(Query):
 	"""
@@ -9,7 +11,11 @@ def fetch_first_result(Query):
 	SearchResultURL = 'https://www.youtube.com/results?search_query=' + Query.replace(' ', '+')
 	SearchResultHTML = requests.get(SearchResultURL).text
 	SearchResultSoup = BeautifulSoup(SearchResultHTML, 'html.parser')
-	VideoURL = SearchResultSoup.find('a', class_='yt-uix-sessionlink yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2       spf-link ').get('href')
+	try:
+		VideoURL = SearchResultSoup.find('a', class_='yt-uix-sessionlink yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2       spf-link ').get('href')
+	except AttributeError:
+		print("Couldn't find song!")
+		sys.exit()
 	VideoURL = 'https://www.youtube.com' + VideoURL
 	return VideoURL
 
@@ -20,10 +26,28 @@ def download_mp3_file(VideoURL):
 	KeepVidURL = "http://keepvid.com/?url=" + VideoURL
 	KeepVidHTML = requests.get(KeepVidURL).text
 	KeepVidSoup = BeautifulSoup(KeepVidHTML, 'html.parser')
-	Text = KeepVidSoup.find(text = " M4A - 128 kbps ")
+	try:
+		Text = KeepVidSoup.find(text = " M4A - 128 kbps ")
+	except AttributeError:
+		print("Error downloading mp3 file!")
+		sys.exit()
 	DownloadLink = Text.parent.parent.get('href')
-	FileName = KeepVidSoup.h3.string[:40] + ".mp3"
-	wget.download(DownloadLink, FileName)
+	FileName = ' '.join(KeepVidSoup.h3.string.split()[:6]) + ".mp3"
+	print("Downloading {FN}..".format(FN = FileName))
+	try:
+		wget.download(DownloadLink, FileName)
+	except KeyboardInterrupt:
+		print("\nDownload cancelled.")
+		# Deletes partially downloaded file
+		for file in os.listdir():
+			if file.startswith(FileName):
+				os.remove(file)
+		sys.exit()
+	print("\nDownload completed.")
 
-Query = input("Enter the name of song : ")
+try:
+	Query = input("Enter the name of song : ")
+except KeyboardInterrupt:
+	print("\nProgram Stopped!")
+	sys.exit()
 download_mp3_file(fetch_first_result(Query))
